e0fb863dcb57297c0da7389c8e508660f55ec722
sourav-dut <atomx0k@gmail.com>
2025-04-07 01:38:54 +0530

    Production ready cartzone

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const app = express();
 import cors from 'cors'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
+import path from 'path';
 import { dbConnection } from '../config/dbConnection.js';
 
 // files
@@ -27,10 +28,15 @@ dbConnection(process.env.MONGO_URL).then(()=> {
     console.log("DB Connected")
 }).catch((err) => {
     console.log(err);
-})
+});
+
+const _dirname = path.resolve();
 
 // middlewares
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
@@ -47,6 +53,11 @@ app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/order", orderRouter);
 app.use("/api/v1/wish", wishRouter);
 app.use('/api/upload', uploadRouter);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+});
 
 // listen
 app.listen(PORT, () => console.log(`server [STARTED] ~ http://localhost:${PORT}`));
